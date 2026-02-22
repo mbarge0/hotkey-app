b904dde9435f0d644a67231499f3fa06225f672a
mbarge0 <mbarge0@gmail.com>
2026-02-22 09:47:57 -0600

    Add avatars, media, favicons, and fix .gitignore

---
+++ apps/review/public/generate-favicons.js
@@ -0,0 +1,12 @@
+// Simple script to note: For production, use proper tools
+// For now, SVG will work in modern browsers
+// To generate PNG/ICO properly, use https://realfavicongenerator.net/ 
+// with the favicon.svg file
+
+console.log('SVG favicon created at public/favicon.svg');
+console.log('For best cross-browser support, generate additional formats at:');
+console.log('https://realfavicongenerator.net/');
+console.log('');
+console.log('Upload: public/favicon.svg');
+console.log('Download: all generated files');
+console.log('Place in: public/ directory');
